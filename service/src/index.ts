6f55a32d39b315399262b5c19cb328391fd56145
whatwewant <tobewhatwewant@outlook.com>
2023-03-13 08:45:07 +0800

    feat: support jwt with connect (x-connect-token)

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,14 +1,46 @@
 import express from 'express'
 import doreamon from '@zodash/doreamon'
+import jwt from 'jsonwebtoken';
 import type { ChatContext, ChatMessage } from './chatgpt'
 import { chatConfig, chatReplyProcess } from './chatgpt'
 import { auth } from './middleware/auth'
+
+declare module 'express' {
+  export interface Request {
+    user: User
+  }
+}
+
+interface User {
+  user_id: string,
+  user_nickname: string,
+  user_avatar: string,
+  user_email: string,
+}
 
 const app = express()
 const router = express.Router()
 
 app.use(express.static('public'))
 app.use(express.json())
+
+// @TODO jwt user
+app.use(async (req, res, next) => {
+  if (process.env.SECRET_KEY !== "") {
+    const token = req.get('x-connect-token')
+    if (token != '') {
+      try {
+        (req as any).$user = jwt.verify(token, process.env.SECRET_KEY)
+      } catch (err) {
+        console.error('jwt verify error:', err)
+
+        throw new Error('Invalid JWT Token')
+      }
+    }
+  }
+
+  next()
+})
 
 app.all('*', (_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
@@ -19,6 +51,9 @@ app.all('*', (_, res, next) => {
 
 router.post('/chat-process', auth, async (req, res) => {
   res.setHeader('Content-type', 'application/octet-stream')
+  doreamon.logger.info(`[${req.method} ${req.path}] ${req.get('user-agent')}`)
+
+  const jwtUser: User = (req as any).$user;
 
   const {
     prompt,
@@ -34,7 +69,7 @@ router.post('/chat-process', auth, async (req, res) => {
 
   try {
     let firstChunk = true
-    doreamon.logger.info(`${user?.nickname} ask ChatGPT: ${prompt}`)
+    doreamon.logger.info(`${user?.nickname}(jwt: ${jwtUser?.user_nickname}) ask ChatGPT: ${prompt}`)
 
     await chatReplyProcess(prompt, options, (chat: ChatMessage) => {
       // doreamon.logger.info(`ChatGPT answer ${user?.nickname}: ${prompt}`);
@@ -42,10 +77,10 @@ router.post('/chat-process', auth, async (req, res) => {
       firstChunk = false
     })
 
-    doreamon.logger.info(`ChatGPT answer ${user?.nickname}: success for prompt => ${prompt}`)
+    doreamon.logger.info(`ChatGPT answer ${user?.nickname}(jwt: ${jwtUser?.user_nickname}): success for prompt => ${prompt}`)
   }
   catch (error) {
-    doreamon.logger.error(`ChatGPT answer ${user?.nickname}: error for prompt => ${prompt}, error detail:`, error)
+    doreamon.logger.error(`ChatGPT answer ${user?.nickname}(jwt: ${jwtUser?.user_nickname}): error for prompt => ${prompt}, error detail:`, error)
     // @TODO context_length_exceeded
     if (error.message.includes('context_length_exceeded')) {
       res.write('对话内容长度异常，请勿提问过快，通过 New Chat 创建新对话即可解决问题')
